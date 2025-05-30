32493cbdc29c6a34f64f6c8416208081465caec9
TariqKichawele <tariqkichawele66@gmail.com>
2025-05-30 11:05:09 -0400

    Enhance CompanionsList component with table structure and styling; update main page to display recently completed sessions.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import CompanionCard from "@/components/CompanionCard";
 import CTA from "@/components/CTA";
 import CompanionsList from "@/components/CompanionsList";
 import React from "react";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
@@ -36,7 +37,11 @@ const Page = () => {
       </section>
 
       <section className="home-section">
-        <CompanionsList />
+        <CompanionsList 
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
         <CTA />
       </section>
     </main>
